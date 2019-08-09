31cb90d940a2ca1c3a8040cf41febbd3c3d0cb48
cosven <yinshaowen241@gmail.com>
2019-08-10 01:40:37 +0800

    defer snails

--- elisp/init-snails.el
+++ elisp/init-snails.el
@@ -1,18 +1,46 @@
+;;; init-snails.el --- snails -*- lexical-binding: nil; -*-
+;;; Commentary:
+;;; Requires
 
 ;;; Code:
 
+;; 为了加快启动速度，defer snails
 (use-package snails
+  :defer t
   :load-path "third_party/snails/"
+  :init
+  (autoload 'snails "snails" nil t)
   :bind
   (:map global-map
         ("M-p" . (lambda ()
                    (interactive)
-                   (snails '(snails-backend-projectile
+                   (snails '(snails-backend-projects
+                             snails-backend-projectile
                              snails-backend-buffer
-                             snails-backend-imenu
-                             snails-backend-recentf
-                             snails-backend-awesome-tab-group)))))
-  )
+                             snails-backend-recentf))))))
+
+(eval-after-load 'snails
+  '(snails-create-sync-backend
+    :name
+    "PROJECTS"
+
+    :candidate-filter
+    (lambda (input)
+      (let ((candidates)
+            (projects (projectile-relevant-known-projects)))
+        (message input)
+        (when projects
+          (dolist (project projects)
+            (when (or
+                   (string-equal input "")
+                   (snails-match-input-p input project))
+              (snails-add-candiate 'candidates (snails-wrap-file-icon project) project))))
+        (snails-sort-candidates input candidates 1 1)
+        candidates))
+
+    :candiate-do
+    (lambda (candidate)
+      (projectile-switch-project-by-name candidate))))
 
 (provide 'init-snails)
 
