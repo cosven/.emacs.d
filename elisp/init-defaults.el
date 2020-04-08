2535160218ca070f00be8b1eba72c71acce764b7
cosven <yinshaowen241@gmail.com>
2020-04-08 22:09:29 +0800

    upgrade awesome-tab/snails

--- elisp/init-defaults.el
+++ elisp/init-defaults.el
@@ -38,12 +38,11 @@
 (electric-pair-mode -1)
 (show-paren-mode 1)
 (which-function-mode -1)
-(global-display-line-numbers-mode)
 (recentf-mode)
 
 (when (eq system-type 'darwin) ;; mac specific settings
   (progn
-    (setq mac-option-modifier 'super)
+    (setq mac-option-modifier 'meta)
     (setq mac-command-modifier 'meta)
     )
   (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
