376c5e847334768d5ee2a602c685fc44c2107280
cosven <yinshaowen241@gmail.com>
2021-01-01 19:21:51 +0800

    add keybindings

--- elisp/init-evil.el
+++ elisp/init-evil.el
@@ -4,17 +4,20 @@
 
 ;; https://github.com/noctuid/evil-guide
 (use-package evil
-  :defer t
   :ensure t
   :custom
   (evil-disable-insert-state-bindings t)
-  (evil-insert-state-cursor 'box)
+  ;; (evil-insert-state-cursor 'box)
   :init
   :config
-
   ;; define my-leader-map and keybindings
   (defvar my-leader-map (make-sparse-keymap)
     "Keymap for \"leader key\" shortcuts.")
+  (general-evil-setup)
+  (evil-set-leader 'normal "," t)
+  (general-nmap
+    "tt" 'treemacs
+    "f" 'grep-curword)
   (general-nmap
    :prefix "SPC"
    :prefix-map 'my-leader-map
@@ -22,7 +25,8 @@
    "e" '(lambda ()
           (interactive)
           (find-file user-init-file))
-   "o" 'other-window)
+   "o" 'other-window
+   "p" '(:keymap projectile-command-map :package projectile))
 
   (modify-syntax-entry ?_ "w")
   (evil-mode 1))
